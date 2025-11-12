c85f5d1308d80c0e63f25555a9213006fe802211
Zinedean Meaker Saaiman <ST10348753@vcconnect.edu.za>
2025-11-12 18:46:34 +0200

    Implement BinaryTree with pre/in/post-order traversals

--- MunicipalServicesApp/Structures/Trees/BinaryTree.cs
+++ MunicipalServicesApp/Structures/Trees/BinaryTree.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MunicipalServicesApp.Structures.Trees
 {
-    internal class BinaryTree
+    /// <summary>
+    /// Minimal binary tree for traversal demo (not ordered).
+    /// </summary>
+    public class BinaryTreeNode<T>
     {
+        public T Value;
+        public BinaryTreeNode<T> Left, Right;
+        public BinaryTreeNode(T value) { Value = value; }
+    }
+
+    public static class BinaryTree
+    {
+        public static void PreOrder<T>(BinaryTreeNode<T> n, Action<T> visit)
+        {
+            if (n == null) return;
+            visit(n.Value);
+            PreOrder(n.Left, visit);
+            PreOrder(n.Right, visit);
+        }
+        public static void InOrder<T>(BinaryTreeNode<T> n, Action<T> visit)
+        {
+            if (n == null) return;
+            InOrder(n.Left, visit);
+            visit(n.Value);
+            InOrder(n.Right, visit);
+        }
+        public static void PostOrder<T>(BinaryTreeNode<T> n, Action<T> visit)
+        {
+            if (n == null) return;
+            PostOrder(n.Left, visit);
+            PostOrder(n.Right, visit);
+            visit(n.Value);
+        }
     }
 }
